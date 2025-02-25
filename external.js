6bbccd5c313c6f5207e6f753eb3e28799e8a2870
Doron Pela <devronpela@gmail.com>
2025-02-25 04:14:33 +0000

    Declare getComputerChoice function

---
+++ external.js
@@ -0,0 +1,16 @@
+const getComputerChoice = () =>{
+    let choice;
+    let n = Math.random()*0.9;
+
+    if (n>=0 && n<0.3){
+        choice = 'rock';
+    }else if (n>=0.3 && n<0.6){
+        choice = 'paper';
+    }else{
+        choice ='scissors';
+    }
+    return choice;
+}
+
+getComputerChoice();
+
